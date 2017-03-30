3e97ce74712a32a9c8a0f773873e8e555cd578af
bizimunda <yourbelover@gmail.com>
2017-03-30 11:16:54 +0200

    Initial

---
+++ Logging/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EventLog eventLog = new EventLog();
+            if (!EventLog.SourceExists("mySource1"))
+            {
+                EventLog.CreateEventSource("mySource", "Application");
+            }
+
+            eventLog.Source = "mySource";
+            int eventID = 8;
+            eventLog.WriteEntry("my message", EventLogEntryType.Warning, eventID);
+            Console.WriteLine("Done");
+            Console.ReadKey();
+            
+           
+        }
+    }
+}
